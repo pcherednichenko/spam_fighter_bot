2072b9b52248b224f7701b775750f8cf5712f8d2
Pavel <paulcheredn@gmail.com>
2021-04-23 23:22:17 +0200

    update check for other messages

--- internal/app/handler/others.go
+++ internal/app/handler/others.go
@@ -9,11 +9,12 @@ import (
 
 func Other(l *zap.SugaredLogger, b *tb.Bot, s data.Storage) func(m *tb.Message) {
 	return func(m *tb.Message) {
-		if _, ok := s.Exist(m.Chat, m.Sender); ok {
-			err := b.Delete(m)
-			if err != nil {
-				l.Errorf("error while deleting video: %v", err)
-			}
+		if _, ok := s.Exist(m.Chat, m.Sender); !ok {
+			return
+		}
+		err := b.Delete(m)
+		if err != nil {
+			l.Errorf("error while deleting spam message: %v", err)
 		}
 	}
 }
